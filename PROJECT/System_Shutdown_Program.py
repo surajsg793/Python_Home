b916f0fc4dd3bb166f334a27a3c7bae00e9d59b3
Suraj <suraj.sg793@gmail.com>
2023-09-04 14:32:26 +0530

    Some changes in ShutDown Program

--- PROJECT/System_Shutdown_Program.py
+++ PROJECT/System_Shutdown_Program.py
@@ -1,4 +1,17 @@
+# import os
+# def shutdown_PC():
+#     os.system("shutdown /s /t 10")
+# shutdown_PC()
+
 import os
-def shutdown_PC():
-    os.system("shutdown /s /t 10")
-shutdown_PC()
+
+def shutdown():
+    if os.name == 'posix':  # Linux/Unix
+        os.system("shutdown -h now")
+    elif os.name == 'nt':  # Windows
+        os.system("shutdown /s /t 0")
+    else:
+        print("Sorry, unsupported operating system.")
+
+if __name__ == "__main__":
+    shutdown()
