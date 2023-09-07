787d19e08efb3170165359a168e8d885bf26e136
Suraj <suraj.sg793@gmail.com>
2023-09-07 10:21:58 +0530

    Math Module Done

---
+++ PYTHON/Math_19.py
@@ -0,0 +1,21 @@
+x = (2,4,5,6,1,3,7)
+print(min(x))
+print(max(x))
+
+y = -4.6
+print(abs(y))
+
+for i in x :
+    z = pow(i, 3)
+    print(z)
+
+import math
+for i in x :
+    a = math.sqrt(i)
+    print(a)
+
+# b = math.ceil(1.4)
+print(math.ceil(y))
+print(math.floor(y))
+print(math.pi)
+
