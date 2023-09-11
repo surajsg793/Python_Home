c8c695b7a3c78402080d833f4a6256a47266f6da
Suaraj <suraj.sg793@gmail.com>
2023-09-11 17:44:27 +0530

    Done

--- PROJECT/Voice_Assistant.py
+++ PROJECT/Voice_Assistant.py
@@ -19,7 +19,9 @@ def sptext():
 def speechtx():
     engine = pyttsx3.init()
     voices = engine.getProperty('voices')
-    engine.setProperty('voice',voices[0].id)
+    engine.setProperty('voice',voices[1].id)
     rate = engine.getProperty('rate')
-    engine.setProperty('rate', 150)
-    
+    engine.setProperty('rate', 175)
+    engine.say(x)
+    engine.runAndWait()
+speechtx("Hello, Guys !")
